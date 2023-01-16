0ed0a090799432deb4e49f90f17343f87f6d6f69
Fabian Schlager <fabian.schlager@skyr.at>
2023-01-17 00:47:48 +0100

    Add two commands

--- main.ts
+++ main.ts
@@ -1,137 +1,123 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, TFolder, FuzzyMatch, FuzzySuggestModal } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
+class SuggesterModal<T> extends FuzzySuggestModal<T> {
+	private resolved = false;
+	constructor(
+		app: App,
+		private textItems: string[] | ((item: T) => string),
+		private items: T[],
+		private resolve: (value: unknown) => void,
+		private reject: (reason?: any) => void,
+		placeholder: string,
+		limit: number
+	) {
+		super(app);
+		this.setPlaceholder(placeholder);
+		this.limit = limit;
+	}
 
-interface MyPluginSettings {
-	mySetting: string;
+	getItems(): T[] {
+		return this.items;
+	}
+
+	getItemText(item: T): string {
+		if (this.textItems instanceof Function) {
+			return this.textItems(item);
+		}
+		return this.textItems[this.items.indexOf(item)] || "Undefined";
+	}
+
+	onChooseItem(item: T, evt: MouseEvent | KeyboardEvent) {
+		this.resolved = true;
+		this.resolve(item);
+	}
+
+	selectSuggestion(
+		value: FuzzyMatch<T>,
+		evt: MouseEvent | KeyboardEvent
+	): void {
+		this.onChooseSuggestion(value, evt);
+		this.close();
+	}
+
+	onClose(): void {
+		if (!this.resolved) {
+			this.reject("Modal cancelled");
+		}
+	}
+
+	onChooseSuggestion(
+		item: FuzzyMatch<T>,
+		evt: MouseEvent | KeyboardEvent
+	): void {
+		this.onChooseItem(item.item, evt);
+	}
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+export async function suggest<T>(
+	app: App,
+	textItems: string[] | ((item: T) => string),
+	items: T[],
+	placeholder: string,
+	limit: number
+) {
+	return new Promise((resolve, reject) => {
+		new SuggesterModal(
+			app,
+			textItems,
+			items,
+			resolve,
+			reject,
+			placeholder,
+			limit
+		).open();
+	});
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
+export default class QuickPlugin extends Plugin {
 	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+			id: 'create-note-in-folder',
+			name: 'Create Note in Folder',
+			callback: async () => {
+				const folders = this.app.vault.getAllLoadedFiles().filter(i => i instanceof TFolder).map(folder => folder as TFolder)
+				const folder = await suggest(this.app, folders.map(folder => folder.name), folders, "Select target folder ...", 10)
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+				const created_note = await this.app.vault.create(folder + "/Untitled.md", "")
+				const active_leaf = this.app.workspace.activeLeaf;
+				if (!active_leaf) {
+					return;
+				}
+				await active_leaf.openFile(created_note, {
+					state: { mode: "source" },
+				});
+				this.app.workspace.trigger("create", created_note)
+				const view = app.workspace.getActiveViewOfType(MarkdownView);
+				if (view) {
+					const editor = view.editor;
+					editor.focus()
 				}
 			}
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addCommand({
+			id: 'create-note-from-template-in-folder',
+			name: 'Create Note from Template in Folder',
+			checkCallback: (checking: boolean) => {
+				if (checking) {
+					//@ts-ignore
+					return app.plugins.plugins.hasOwnProperty('templater-obsidian')
+				}
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+				const folders = this.app.vault.getAllLoadedFiles().filter(i => i instanceof TFolder).map(folder => folder as TFolder)
+				suggest(this.app, folders.map(folder => folder.name), folders, "Select target folder ...", 10).then(folder => {
+					//@ts-ignore
+					app.plugins.plugins['templater-obsidian'].fuzzy_suggester.create_new_note_from_template(folder)
+				})
+			}
 		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
 	}
 }
